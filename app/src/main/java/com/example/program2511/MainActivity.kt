20f70a603f74315581c3c1e1f970598d4bf6d8a2
kamcio420 <kamcio420@gmail.com>
2021-11-25 12:28:41 +0100

    1 i 2 gigachad

--- app/src/main/java/com/example/program2511/MainActivity.kt
+++ app/src/main/java/com/example/program2511/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.program2511
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button1).setOnClickListener {
+            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.gigachad)
+        }
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            findViewById<ImageView>(R.id.imageView).setImageResource(R.drawable.gigachad2)
+        }
     }
 }
